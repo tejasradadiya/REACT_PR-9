b59aa2ca3d7fe6b9108169ee586294ac9573a8a3
Anant Radadiya <anantradadiya@ANANT-Radadiya-MacBook-Air.local>
2023-09-20 09:10:26 +0530

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,12 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Rudra',
+    lastName: 'Monpara',
+    email: 'Rudra@example.com',
+    salary: '500000',
+    date: '2017-08-2'
+  },
+];
+
+export { employeesData };
